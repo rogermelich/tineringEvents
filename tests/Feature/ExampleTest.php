2e607523e418056c6c48918ddde3e9b48fd47e15
Roger Melich <rogermelich@gmail.com>
2017-02-14 21:24:35 +0100

    Add Register Test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use Event;
+use Illuminate\Auth\Events\Registered;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -20,4 +22,53 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testRegisterUserSendWelcomeEmail()
+    {
+        Mail::fake();
+        $user = new \App\User();
+        $user->name = 'Pepepepito';
+        $user->email = 'pepepepito@gmail.com';
+        event(new Registered($user));
+        Mail::assertSent(WelcomeEmailMarkdown::class,function($mail) use ($user)  {
+            return $mail->user->name ===  $user->name;
+        });
+    }
+    /**
+     *
+     */
+    public function testRegisterUserSendWelcomeEmail2()
+    {
+        Event::fake();
+        $this->get('/registerUser');
+        Event::assertDispatched(Registered::class,function($event)  {
+            return $event->user->name === 'Pepepepito';
+        });
+    }
+
+
+
+//    public function testRegiserUserSendWelcomeEmail()
+//    {
+//        Event::fake();
+//
+//        $this->visit('/register')
+//            ->type('Pepepito', 'name')
+//            ->type('pepepito@gmail.com', 'email')
+//            ->type('1234', 'password')
+//            ->type('1234', 'password_confirmation')
+//            ->press('Register')
+//            ->seePage('/home')
+//            ->seeInDatabase('users',
+//                ['email' => 'pepepit@gmail.com',
+//                'name' => 'Pepepito'
+//            ]);
+//
+//        $user = '';
+//
+//        Event::assertDispatched(Registered::class, function ($event) use ($user) {
+//            return $event->$user->name == 'Pepepito';
+//        });
+//
+//    }
 }
