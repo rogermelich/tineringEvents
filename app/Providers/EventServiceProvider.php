576d926f450252a957785bcbeaac6358d9d3bed4
Roger Melich <rogermelich@gmail.com>
2017-02-14 20:07:14 +0100

    Add hook to Illuminate\Auth\Events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,7 +16,7 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\SomeEvent' => [
             'App\Listeners\EventListener',
         ],
-        'App\Events\NewRegisteredUserEvents' => [
+        'Illuminate\Auth\Events\Register' => [
             'App\Listeners\WelcomeEmailListener',
         ],
     ];
